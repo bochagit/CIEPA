0dd809b4646100f790db7c7551273bbe76d1a190
bochagit <gocardozo@alumno.unlam.edu.ar>
2025-11-05 12:33:06 -0300

    Agregando CORS especifico para mas seguridad y cambiando las rutas de /api a /

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,15 +9,19 @@ import youtubeRoutes from './routes/youtube.routes.js'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173/', 'https://ciepa.onrender.com/'],
+    credentials: true
+}))
+
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 app.use(morgan("dev"))
 
-app.use("/api/auth", authRoutes)
-app.use("/api/posts", postRoutes)
-app.use("/api/upload", uploadRoutes)
-app.use("/api/categories", categoryRoutes)
-app.use("/api/youtube", youtubeRoutes)
+app.use("/auth", authRoutes)
+app.use("/posts", postRoutes)
+app.use("/upload", uploadRoutes)
+app.use("/categories", categoryRoutes)
+app.use("/youtube", youtubeRoutes)
 
 export default app
